4160c0c0cb7d429e1c3ee9cfabd47ecf8f53230b
mominahmed23 <mominahmad324@gmail.com>
2021-06-18 17:01:25 +0500

    First Commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,14 @@
-import logo from './logo.svg';
+import { Button, Typography } from '@material-ui/core';
 import './App.css';
+import Layout from './components/Layout';
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+ return (
+   <Layout>
+     <Typography variant="h4">Hello world</Typography>
+     <Button variant="outlined">Click</Button>
+   </Layout>
+ );
 }
 
 export default App;
