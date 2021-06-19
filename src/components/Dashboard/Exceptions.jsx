16304935b86aba4b6e8015ae0e4d9d90d1036762
ravender26 <ravender.jadon123@gmail.com>
2021-06-19 17:08:39 +0530

    table changes

--- src/components/Dashboard/Exceptions.jsx
+++ src/components/Dashboard/Exceptions.jsx
@@ -1,109 +1,114 @@
-import React from 'react';
-import { makeStyles } from '@material-ui/core/styles';
-import { createMuiTheme, ThemeProvider } from '@material-ui/core/styles';
-import Table from '@material-ui/core/Table';
-import TableBody from '@material-ui/core/TableBody';
-import TableCell from '@material-ui/core/TableCell';
-import TableContainer from '@material-ui/core/TableContainer';
-import TableHead from '@material-ui/core/TableHead';
-import TableRow from '@material-ui/core/TableRow';
-import Paper from '@material-ui/core/Paper';
-import Switch from '@material-ui/core/Switch';
-import { green } from '@material-ui/core/colors';
-import { InputBase, Link } from '@material-ui/core';
-import { Typography } from '@material-ui/core';
-import SearchIcon from '@material-ui/icons/Search';
+import { useState } from "react";
+import { makeStyles } from "@material-ui/core/styles";
+import { createMuiTheme, ThemeProvider } from "@material-ui/core/styles";
+import Table from "@material-ui/core/Table";
+import TableBody from "@material-ui/core/TableBody";
+import TableCell from "@material-ui/core/TableCell";
+import TableContainer from "@material-ui/core/TableContainer";
+import TableHead from "@material-ui/core/TableHead";
+import TableRow from "@material-ui/core/TableRow";
+import Paper from "@material-ui/core/Paper";
+import Switch from "@material-ui/core/Switch";
+import { green } from "@material-ui/core/colors";
+import { InputBase, Link } from "@material-ui/core";
+import { Typography } from "@material-ui/core";
+import SearchIcon from "@material-ui/icons/Search";
+
 const useStyles = makeStyles({
   table: {
     minWidth: 650,
-    border:'1px solid #e1e1e1'
+    border: "1px solid #e1e1e1",
   },
-  wrap:{
-    marginBottom:'30px',
-    border:'1px solid #e1e1e1',
-    padding:'20px'
+  wrap: {
+    marginBottom: "30px",
+    border: "1px solid #e1e1e1",
+    padding: "20px",
   },
-  column:{
-    border:'1px solid #e1e1e1'
+  column: {
+    border: "1px solid #e1e1e1",
   },
-  searchBar:{
-      border:'1px solid #c9c9c9',
-      borderRadius:'5px',
-      padding:'2px 8px',
-      minWidth:'300px'
-
+  emptyColumn: {
+    height: "40px",
   },
-  titleWrap:{
-      display:'flex',
-      justifyContent:'space-between',
-      alignItems:'center',
-      marginBottom:'20px'
-  }
+  newColumn: {
+    border: "1px solid #e1e1e1",
+    backgroundColor: "#fce9dd",
+  },
+  searchBar: {
+    border: "1px solid #c9c9c9",
+    borderRadius: "5px",
+    padding: "2px 8px",
+    minWidth: "300px",
+  },
+  titleWrap: {
+    display: "flex",
+    justifyContent: "space-between",
+    alignItems: "center",
+    marginBottom: "20px",
+  },
 });
 const outerTheme = createMuiTheme({
-    palette: {
-      secondary: {
-        main: green[500],
-      },
+  palette: {
+    secondary: {
+      main: green[500],
     },
-  });
+  },
+});
 const rows = [
-    {
-        name:"1",
-        user:"1",
-        key1:1,
-        key2:2,
-        key3:3,
-        key4:4,
-        key5:5,
-    },
-    {
-        name:"2",
-        user:"1",
-        key1:1,
-        key2:2,
-        key3:3,
-        key4:4,
-        key5:5,
-    },
-    {
-        name:"3",
-        user:"1",
-        key1:1,
-        key2:2,
-        key3:3,
-        key4:4,
-        key5:5,
-    },
-    {
-        name:"4",
-        user:"1",
-        key1:1,
-        key2:2,
-        key3:3,
-        key4:4,
-        key5:5,
-    },
-    {
-        name:"5",
-        user:"1",
-        key1:1,
-        key2:2,
-        key3:3,
-        key4:4,
-        key5:5,
-    },
-    {
-        name:"6",
-        user:"1",
-        key1:1,
-        key2:2,
-        key3:3,
-        key4:4,
-        key5:5,
-    },
+  {
+    User: "Thomas K",
+    name: "Sales Rep",
+    key1: 1,
+    key2: 2,
+    key3: 3,
+    key4: 4,
+    key5: 5,
+  },
+  {
+    User: "Antony A",
+    name: "Account Manager",
+    key1: 1,
+    key2: 2,
+    key3: 3,
+    key4: 4,
+    key5: 5,
+  },
+  {
+    User: "Jerry L",
+    name: "Management",
+    key1: 1,
+    key2: 2,
+    key3: 3,
+    key4: 4,
+    key5: 5,
+  },
+  {
+    User: "Lisa K",
+    name: "Finance Manager",
+    key1: 1,
+    key2: 2,
+    key3: 3,
+    key4: 4,
+    key5: 5,
+  },
+  {
+    User: "John S",
+    name: "Business Admin",
+    key1: 1,
+    key2: 2,
+    key3: 3,
+    key4: 4,
+    key5: 5,
+  },
 ];
 export default function Exceptions() {
+  const [newColor, setNewColor] = useState({
+    border: "1px solid #e1e1e1",
+    backgroundColor: "#edc19d",
+  });
+
+  console.log(newColor);
+
   const classes = useStyles();
   return (
     <>
@@ -128,37 +133,91 @@ export default function Exceptions() {
         <Table className={classes.table} aria-label="simple table">
           <TableHead>
             <TableRow>
-            <TableCell className={classes.column}>User</TableCell>
-            <TableCell className={classes.column}>Role</TableCell>
-            <TableCell className={classes.column} align="right">Growth Drives</TableCell>
-            <TableCell className={classes.column} align="right">Price Metrics</TableCell>
-            <TableCell className={classes.column} align="right">Profit Analytics</TableCell>
-            <TableCell className={classes.column} align="right">Money Makers</TableCell>
-            <TableCell className={classes.column} align="right">Loss Makers</TableCell>
+              <TableCell className={classes.column}>User</TableCell>
+              <TableCell className={classes.column}>Role</TableCell>
+              <TableCell className={classes.column} align="center">
+                Growth Drives
+              </TableCell>
+              <TableCell className={classes.column} align="center">
+                Price Metrics
+              </TableCell>
+              <TableCell className={classes.column} align="center">
+                Profit Analytics
+              </TableCell>
+              <TableCell className={classes.column} align="center">
+                Money Makers
+              </TableCell>
+              <TableCell className={classes.column} align="center">
+                Loss Makers
+              </TableCell>
             </TableRow>
           </TableHead>
           <TableBody>
-              <TableRow>
-            <TableCell className={classes.column} style={{borderRight:"none"}}>
-              <Link href="#" onClick={(e) => e.preventDefault()}>
-                +Add User
-              </Link>
-            </TableCell>
+            <TableRow className={classes.emptyColumn}>
+              <TableCell className={classes.column} component="th" scope="row">
+                <Link href="#" onClick={(e) => e.preventDefault()} class="link">
+                  +Add User
+                </Link>
+              </TableCell>
+              <ThemeProvider theme={outerTheme}>
+                <TableCell
+                  className={classes.column}
+                  align="center"
+                ></TableCell>
+                <TableCell
+                  className={classes.column}
+                  align="center"
+                ></TableCell>
+                <TableCell
+                  className={classes.column}
+                  align="center"
+                ></TableCell>
+                <TableCell
+                  className={classes.column}
+                  align="center"
+                ></TableCell>
+                <TableCell
+                  className={classes.column}
+                  align="center"
+                ></TableCell>
+                <TableCell
+                  className={classes.column}
+                  align="center"
+                ></TableCell>
+              </ThemeProvider>
             </TableRow>
             {rows.map((row) => (
               <TableRow key={row.name}>
-                 <TableCell className={classes.column} component="th" scope="row">
-                  {row.user}
+                <TableCell
+                  className={classes.column}
+                  component="th"
+                  scope="row"
+                >
+                  {row.User}
                 </TableCell>
-                <TableCell className={classes.column} component="th" scope="row">
-                {row.name}
-              </TableCell>
+                <TableCell
+                  className={classes.column}
+                  component="th"
+                  scope="row"
+                >
+                  {row.name}
+                </TableCell>
                 <ThemeProvider theme={outerTheme}>
-                <TableCell className={classes.column} align="right"><Switch className="switchColor"/></TableCell>
-                <TableCell className={classes.column} align="right"><Switch/></TableCell>
-                <TableCell className={classes.column} align="right"><Switch/></TableCell>
-                <TableCell className={classes.column} align="right"><Switch/></TableCell>
-                <TableCell className={classes.column} align="right"><Switch/></TableCell>
+                  <TableCell className={classes.column} align="center">
+                    <Switch className="switchColor" />
+                  </TableCell>
+                  <TableCell className={classes.column} align="center">
+                    <Switch />
+                  </TableCell>
+                  <TableCell className={classes.column} align="center">
+                    <Switch />
+                  </TableCell>
+                  <TableCell className={classes.column} align="center">
+                    <Switch />
+                  </TableCell>
+                  <TableCell className={classes.newColumn} align="center">
+                    <Switch />
+                  </TableCell>
                 </ThemeProvider>
               </TableRow>
             ))}
--- src/components/Dashboard/RolesAndPrivileges.jsx
+++ src/components/Dashboard/RolesAndPrivileges.jsx
@@ -1,130 +1,179 @@
-import React from 'react';
-import { makeStyles } from '@material-ui/core/styles';
-import { createMuiTheme, ThemeProvider } from '@material-ui/core/styles';
-import Table from '@material-ui/core/Table';
-import TableBody from '@material-ui/core/TableBody';
-import TableCell from '@material-ui/core/TableCell';
-import TableContainer from '@material-ui/core/TableContainer';
-import TableHead from '@material-ui/core/TableHead';
-import TableRow from '@material-ui/core/TableRow';
-import Paper from '@material-ui/core/Paper';
-import Switch from '@material-ui/core/Switch';
-import { green } from '@material-ui/core/colors';
-import { Link } from '@material-ui/core';
-import { Typography } from '@material-ui/core';
+import React from "react";
+import { makeStyles } from "@material-ui/core/styles";
+import { createMuiTheme, ThemeProvider } from "@material-ui/core/styles";
+import Table from "@material-ui/core/Table";
+import TableBody from "@material-ui/core/TableBody";
+import TableCell from "@material-ui/core/TableCell";
+import TableContainer from "@material-ui/core/TableContainer";
+import TableHead from "@material-ui/core/TableHead";
+import TableRow from "@material-ui/core/TableRow";
+import Paper from "@material-ui/core/Paper";
+import Switch from "@material-ui/core/Switch";
+import { green } from "@material-ui/core/colors";
+import { Link } from "@material-ui/core";
+import { Typography } from "@material-ui/core";
 const useStyles = makeStyles({
   table: {
     minWidth: 650,
-    border:'1px solid #e1e1e1'
+    border: "1px solid #e1e1e1",
   },
-  wrap:{
-    marginBottom:'30px',
-    border:'1px solid #e1e1e1',
-    padding:'20px'
+  wrap: {
+    marginBottom: "30px",
+    border: "1px solid #e1e1e1",
+    padding: "20px",
   },
-  column:{
-    border:'1px solid #e1e1e1'
-  }
+  column: {
+    border: "1px solid #e1e1e1",
+  },
+  emptyColumn: {
+    height: "40px",
+  },
+  
 });
 const outerTheme = createMuiTheme({
-    palette: {
-      secondary: {
-        main: green[500],
-      },
+  palette: {
+    secondary: {
+      main: green[500],
     },
-  });
+  },
+});
 const rows = [
-    {
-        name:"1",
-        key1:1,
-        key2:2,
-        key3:3,
-        key4:4,
-        key5:5,
-    },
-    {
-        name:"2",
-        key1:1,
-        key2:2,
-        key3:3,
-        key4:4,
-        key5:5,
-    },
-    {
-        name:"3",
-        key1:1,
-        key2:2,
-        key3:3,
-        key4:4,
-        key5:5,
-    },
-    {
-        name:"4",
-        key1:1,
-        key2:2,
-        key3:3,
-        key4:4,
-        key5:5,
-    },
-    {
-        name:"5",
-        key1:1,
-        key2:2,
-        key3:3,
-        key4:4,
-        key5:5,
-    },
-    {
-        name:"6",
-        key1:1,
-        key2:2,
-        key3:3,
-        key4:4,
-        key5:5,
-    },
+  {
+    User: "Thomas K",
+    name: "Sales Rep",
+    key1: 1,
+    key2: 2,
+    key3: 3,
+    key4: 4,
+    key5: 5,
+  },
+  {
+    User: "Antony A",
+    name: "Account Manager",
+    key1: 1,
+    key2: 2,
+    key3: 3,
+    key4: 4,
+    key5: 5,
+  },
+  {
+    User: "Jerry L",
+    name: "Management",
+    key1: 1,
+    key2: 2,
+    key3: 3,
+    key4: 4,
+    key5: 5,
+  },
+  {
+    User: "Lisa K",
+    name: "Finance Manager",
+    key1: 1,
+    key2: 2,
+    key3: 3,
+    key4: 4,
+    key5: 5,
+  },
+  {
+    User: "John S",
+    name: "Business Admin",
+    key1: 1,
+    key2: 2,
+    key3: 3,
+    key4: 4,
+    key5: 5,
+  },
 ];
 export default function RolesAndPrivileges() {
   const classes = useStyles();
   return (
     <>
-    <Typography variant="h5">Roles and Privileges</Typography>
-    <TableContainer className={classes.wrap} component={Paper}>
-      <Table className={classes.table} aria-label="simple table">
-        <TableHead>
-          <TableRow>
-            <TableCell className={classes.column}>Role</TableCell>
-            <TableCell className={classes.column} align="right">Growth Drives</TableCell>
-            <TableCell className={classes.column} align="right">Price Metrics</TableCell>
-            <TableCell className={classes.column} align="right">Profit Analytics</TableCell>
-            <TableCell className={classes.column} align="right">Money Makers</TableCell>
-            <TableCell className={classes.column} align="right">Loss Makers</TableCell>
-          </TableRow>
-        </TableHead>
-        <TableBody>
-        <TableRow>
-            <TableCell className={classes.column} style={{borderRight:"none"}}>
-              <Link href="#" onClick={(e) => e.preventDefault()}>
-                +Add Role
-              </Link>
-            </TableCell>
+      <Typography variant="h5">Roles and Privileges</Typography>
+      <TableContainer className={classes.wrap} component={Paper}>
+        <Table className={classes.table} aria-label="simple table">
+          <TableHead>
+            <TableRow>
+              <TableCell className={classes.column}>Role</TableCell>
+              <TableCell className={classes.column} align="center">
+                Growth Drives
+              </TableCell>
+              <TableCell className={classes.column} align="center">
+                Price Metrics
+              </TableCell>
+              <TableCell className={classes.column} align="center">
+                Profit Analytics
+              </TableCell>
+              <TableCell className={classes.column} align="center">
+                Money Makers
+              </TableCell>
+              <TableCell className={classes.column} align="center">
+                Loss Makers
+              </TableCell>
             </TableRow>
-          {rows.map((row) => (
-            <TableRow key={row.name}>
-              <TableCell className={classes.column} component="th" scope="row">
-                {row.name}
+          </TableHead>
+          <TableBody>
+            <TableRow className={classes.emptyColumn}>
+              <TableCell className={classes.column}
+                  component="th"
+                  scope="row">
+                <Link href="#" onClick={(e) => e.preventDefault()}  class="link">
+                  +Add Role
+                </Link>
               </TableCell>
               <ThemeProvider theme={outerTheme}>
-                <TableCell className={classes.column} align="right"><Switch className="switchColor"/></TableCell>
-                <TableCell className={classes.column} align="right"><Switch/></TableCell>
-                <TableCell className={classes.column} align="right"><Switch/></TableCell>
-                <TableCell className={classes.column} align="right"><Switch/></TableCell>
-                <TableCell className={classes.column} align="right"><Switch/></TableCell>
+                <TableCell
+                  className={classes.column}
+                  align="center"
+                ></TableCell>
+                <TableCell
+                  className={classes.column}
+                  align="center"
+                ></TableCell>
+                <TableCell
+                  className={classes.column}
+                  align="center"
+                ></TableCell>
+                <TableCell
+                  className={classes.column}
+                  align="center"
+                ></TableCell>
+                <TableCell
+                  className={classes.column}
+                  align="center"
+                ></TableCell>
               </ThemeProvider>
             </TableRow>
-          ))}
-        </TableBody>
-      </Table>
-    </TableContainer>
+            {rows.map((row) => (
+              <TableRow key={row.name}>
+                <TableCell
+                  className={classes.column}
+                  component="th"
+                  scope="row"
+                >
+                  {row.name}
+                </TableCell>
+                <ThemeProvider theme={outerTheme}>
+                  <TableCell className={classes.column} align="center">
+                    <Switch className="switchColor" />
+                  </TableCell>
+                  <TableCell className={classes.column} align="center">
+                    <Switch />
+                  </TableCell>
+                  <TableCell className={classes.column} align="center">
+                    <Switch />
+                  </TableCell>
+                  <TableCell className={classes.column} align="center">
+                    <Switch />
+                  </TableCell>
+                  <TableCell className={classes.column} align="center">
+                    <Switch />
+                  </TableCell>
+                </ThemeProvider>
+              </TableRow>
+            ))}
+          </TableBody>
+        </Table>
+      </TableContainer>
     </>
   );
 }
